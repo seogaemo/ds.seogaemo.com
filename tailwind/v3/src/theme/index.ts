4ae323d4fc7c34c513ed4315bb1417e07604fe67
suk-6 <me@suk.kr>
2025-03-01 15:18:18 +0900

    feat(theme): integrate background and content configurations into theme

--- tailwind/v3/src/theme/index.ts
+++ tailwind/v3/src/theme/index.ts
@@ -2,10 +2,15 @@ import type { Config } from "tailwindcss";
 
 import colors from "./colors";
 import typography from "./typography";
+import background from "./background";
+import content from "./content";
 
 const theme: Pick<Config, "theme"> = {
   theme: {
+    ...content,
     ...typography,
+    ...background,
+
     extend: {
       ...colors,
     },
