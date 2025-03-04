613d42f8377aed867e1facb1da2d3f549c5b4e05
suk-6 <me@suk.kr>
2025-03-04 20:17:58 +0900

    feat(package): bump version to 1.0.4 and include border theme in configuration

--- tailwind/v3/src/theme/index.ts
+++ tailwind/v3/src/theme/index.ts
@@ -3,12 +3,14 @@ import type { Config } from "tailwindcss";
 import colors from "./colors";
 import typography from "./typography";
 import background from "./background";
+import border from "./border";
 
 const theme: Pick<Config, "theme"> = {
   theme: {
     ...colors,
     ...typography,
     ...background,
+    ...border,
   },
 };
 
